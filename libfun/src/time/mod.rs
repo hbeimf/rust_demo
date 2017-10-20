533227a6e9d469a05c29be6e7adcdc39792d3fb6
mao <5622876@qq.com>
2017-10-20 01:39:29 -0700

    timer

--- libfun/src/time/mod.rs
+++ libfun/src/time/mod.rs
@@ -1,3 +1,6 @@
+// https://doc.rust-lang.org/time/time/index.html
+// http://www.cnblogs.com/chenxizhang/p/4760087.html
+
 pub fn test() -> String {
     println!("test time mod!! ===============");
     "test".to_string()

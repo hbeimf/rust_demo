71bb2098450899383384b9766fad3e6dbddcbaed
mao <5622876@qq.com>
2017-10-20 01:00:13 -0700

    demo

--- libfun/src/string/mod.rs
+++ libfun/src/string/mod.rs
@@ -1,11 +1,15 @@
 // https://doc.rust-lang.org/std/string/struct.String.html
+// https://doc.rust-lang.org/std/primitive.str.html
 
 use std::string::String;
 
 
 pub fn test() -> String {
     println!("test string mod!! ===============");
-    let new_str = replace("hello world!", "hello", "hi");
+    let from_string = "hello world!";
+    let find = "hello";
+    let to = "hi";
+    let new_str = replace(from_string, find, to);
     println!("new str: {}", new_str);
 
 
@@ -14,8 +18,8 @@ pub fn test() -> String {
 }
 
 pub fn replace(from_string: &str, find: &str, to: &str) -> String {
-    let s = String::from(from_string);
-    s.replace(find, to)
+    // let s = String::from(from_string);
+    from_string.replace(find, to)
 }
 
 

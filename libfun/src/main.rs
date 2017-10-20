eae52341fda669470287e13b8108926d1056ce2a
mao <5622876@qq.com>
2017-10-19 19:51:52 -0700

    libfun

---
+++ libfun/src/main.rs
@@ -0,0 +1,6 @@
+extern crate libfun;
+
+fn main() {
+    println!("Hello, world!");
+    libfun::string::phplike::test();
+}
---
+++ libfun/src/string/phplike.rs
@@ -0,0 +1,22 @@
+pub struct Obj {
+
+}
+
+// pub fu new() {
+
+// }
+
+pub fn test() -> String {
+    println!("test mod!!");
+    "test".to_string()
+}
+
+
+
+// pub fun trim(inputStr: String) -> String {
+//     inputStr;
+// }
+
+
+
+

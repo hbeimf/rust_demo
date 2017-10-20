1aa450bf7e4c8fbec295c6d8c569b4d2a4e2f10b
mao <5622876@qq.com>
2017-10-20 00:45:49 -0700

    string

--- libfun/src/string/mod.rs
+++ libfun/src/string/mod.rs
@@ -1,4 +1,22 @@
+// https://doc.rust-lang.org/std/string/struct.String.html
+
+use std::string::String;
+
+
 pub fn test() -> String {
     println!("test string mod!!");
+    let new_str = replace("hello world!", "hello", "hi");
+    println!("new str: {}", new_str);
+
+
+
     "test".to_string()
 }
+
+pub fn replace(from_string: &str, find: &str, to: &str) -> String {
+    let s = String::from(from_string);
+    s.replace(find, to)
+}
+
+
+

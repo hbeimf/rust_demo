00f0863b51232ecdbec10876d562790ce6dc779c
mao <5622876@qq.com>
2017-11-01 20:47:10 -0700

    learn

--- libfun/src/main.rs
+++ libfun/src/main.rs
@@ -2,10 +2,10 @@ extern crate libfun;
 
 fn main() {
     println!("Hello, world!");
-    libfun::string::test();
-    libfun::time::test();
-    libfun::learn::test();
-    libfun::stack::test();
-    libfun::queue::test();
+    // libfun::string::test();
+    // libfun::time::test();
+    libfun::learn::test::test();
+    // libfun::stack::test();
+    // libfun::queue::test();
     
 }

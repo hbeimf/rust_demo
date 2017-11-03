789251c5a28483ef79a2d3a5b7d4c0fbccaa50fa
mao <5622876@qq.com>
2017-11-03 00:00:51 -0700

    s

--- libfun/src/learn/test.rs
+++ libfun/src/learn/test.rs
@@ -1,12 +1,12 @@
-use learn;
+// use learn;
 use stack;
 
 pub fn test() -> String {
-    println!("test learn mod!! ===============");
-    learn::learn_array::test();
+    // println!("test learn mod!! ===============");
+    // learn::learn_array::test();
     stack::get_min::test();
 
-    learn::learn_trait::test();
+    // learn::learn_trait::test();
     "test".to_string()
 }
 

385f7a1e32c31fea210b9ed07efeaf7cc9a1c6d6
mao <5622876@qq.com>
2017-11-03 18:16:57 -0700

    l

--- libfun/src/learn/test.rs
+++ libfun/src/learn/test.rs
@@ -4,7 +4,9 @@ use stack;
 pub fn test() -> String {
     // println!("test learn mod!! ===============");
     // learn::learn_array::test();
-    stack::get_min::test();
+    // stack::get_min::test();
+    stack::get_sort::test();
+    
 
     // learn::learn_trait::test();
     "test".to_string()
--- libfun/src/stack/mod.rs
+++ libfun/src/stack/mod.rs
@@ -1,2 +1,3 @@
 pub mod base_stack;
 pub mod get_min;
+pub mod get_sort;

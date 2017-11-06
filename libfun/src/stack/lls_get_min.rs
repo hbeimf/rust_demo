664baf97d47afcd9068747625d5a9d1357ebdb64
mao <5622876@qq.com>
2017-11-05 19:16:23 -0800

    l

---
+++ libfun/src/stack/lls_get_min.rs
@@ -0,0 +1,26 @@
+// use stack::linked_list_stack::LinkedListStack;
+
+// pub struct Min<T> {
+//     top: LinkedListStack,
+//     help: LinkedListStack,
+// }
+
+// impl <T> Min<T> {
+//     fn new() -> Min<T> {
+//         Min{ top: LinkedListStack::<T>::new(), help: LinkedListStack::<T>::new() }
+//     }
+
+//     // fn push(&mut self, val: T) {    
+//     //     self.top.push_front(val);
+//     // }
+
+//     // fn pop(&mut self) -> Option<T> {    
+//     //     self.top.pop_front()
+//     // }
+
+// }
+
+
+
+
+
--- libfun/src/stack/mod.rs
+++ libfun/src/stack/mod.rs
@@ -2,4 +2,6 @@ pub mod base_stack;
 pub mod get_min;
 pub mod get_sort;
 pub mod linked_list_stack;
+pub mod lls_get_min;
+
 

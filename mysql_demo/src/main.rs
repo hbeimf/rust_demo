122913392473e248eef8fefdd618ea40b6d609fd
maomao <you@example.com>
2018-01-16 13:48:12 +0800

    demo

--- mysql_demo/src/main.rs
+++ mysql_demo/src/main.rs
@@ -1,33 +1,3 @@
-// extern crate diesel;
-// // extern crate diesel_codegen;
-// extern crate r2d2;
-// extern crate r2d2_diesel;
-
-// use diesel::mysql::MysqlConnection;
-// // use diesel::mysql::*;
-// use r2d2_diesel::ConnectionManager;
-
-// use std::thread;
-
-// pub type Pool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
-
-// pub fn init_pool() -> Pool {
-//     let manager = ConnectionManager::<MysqlConnection>::new("mysql://root:123456@localhost:3306/test");
-//     r2d2::Pool::new(manager).expect("db pool")
-// }
-
-// fn main() {
-// 	let pool = init_pool();
-
-// 	for _ in 0..10i32 {
-// 		let pool = pool.clone();
-// 		thread::spawn(move || {
-// 			let connection = pool.get();
-// 			assert!(connection.is_ok());
-// 		});
-// 	}
-// }
-
 extern crate mysql_demo;
 
 fn main() {

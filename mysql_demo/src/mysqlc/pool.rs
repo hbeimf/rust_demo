09420057bb183a40a8ad6fd4f3d1aeb408118d8d
maomao <you@example.com>
2018-01-16 13:46:55 +0800

    demo

--- mysql_demo/src/mysqlc/pool.rs
+++ mysql_demo/src/mysqlc/pool.rs
@@ -6,24 +6,11 @@ extern crate r2d2_diesel;
 use diesel::mysql::MysqlConnection;
 use r2d2_diesel::ConnectionManager;
 
-use std::thread;
-
 pub type Pool = r2d2::Pool<ConnectionManager<MysqlConnection>>;
 
+
+// 初始化连接池
 pub fn init_pool() -> Pool {
     let manager = ConnectionManager::<MysqlConnection>::new("mysql://root:123456@localhost:3306/test");
     r2d2::Pool::new(manager).expect("db pool")
-}
-
-
-pub fn test() {
-	let pool = init_pool();
-
-	for _ in 0..10i32 {
-		let pool = pool.clone();
-		thread::spawn(move || {
-			let connection = pool.get();
-			assert!(connection.is_ok());
-		});
-	}
 }
--- mysql_demo/src/mysqlc/test.rs
+++ mysql_demo/src/mysqlc/test.rs
@@ -1,10 +1,10 @@
 use std::thread;
 
 use mysqlc;
-use mysqlc::init;
+use mysqlc::pool;
 
 pub fn test_bak() {
-	let pool = init::init_pool();
+	let pool = pool::init_pool();
 
 	for _ in 0..10i32 {
 		let pool = pool.clone();
@@ -16,13 +16,13 @@ pub fn test_bak() {
 }
 
 pub fn test() {
-	let pool = init::init_pool();
+	let pool = pool::init_pool();
 	// let connection = pool.get();
 	// assert!(connection.is_ok());
 
 	match pool.get() {
 	            Ok(conn) => {
-	            		mysqlc::create_post(&conn, "titletest", "body test");
+	            		mysqlc::table_post::create_post(&conn, "titletest", "body test");
 	            },
 	            // Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
 	            _ => {
@@ -41,75 +41,4 @@ pub fn test() {
 
 	
 }
-
-
-
-
-
-
-// pub fn select() {
-
-
-// }
-
-// extern crate diesel;
-// // extern crate mysqlc;
-
-// use mysqlc::*;
-// use mysqlc::models::*;
-// use diesel::prelude::*;
-
-// fn test() {
-//     use mysqlc::schema::posts::dsl::*;
-
-//     let pool = init::init_pool();
-//     let connection = pool.get();
-
-//     // let connection = establish_connection();
-
-
-//     let results = posts
-//         .filter(published.eq(true))
-//         .limit(5)
-//         .load::<Post>(&connection)
-//         .expect("Error loading posts");
-
-//     println!("Displaying {} posts", results.len());
-//     for post in results {
-//         println!("{}", post.title);
-//         println!("-----------\n");
-//         println!("{}", post.body);
-//     }
-// }
-
-
-
-
-
-
-// pub fn test() {
-// 	use mysqlc::schema::posts::dsl::*;
-
-// 	let pool = init_pool();
-
-// 	for _ in 0..10i32 {
-// 		let pool = pool.clone();
-// 		thread::spawn(move || {
-// 			let connection = pool.get();
-// 			assert!(connection.is_ok());
-// 			let results = posts
-// 			        .filter(published.eq(true))
-// 			        .limit(5)
-// 			        .load::<Post>(&connection)
-// 			        .expect("Error loading posts");
-
-// 			 println!("Displaying {} posts", results.len());
-// 			    for post in results {
-// 			        println!("{}", post.title);
-// 			        println!("-----------\n");
-// 			        println!("{}", post.body);
-// 			    }
-// 		});
-// 	}
-// }
 

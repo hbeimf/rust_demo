3c599c9672c28da513f89027433f3f05a45c7323
maomao <you@example.com>
2018-03-06 14:02:14 +0800

    删除多余的code

--- chat/src/codec.rs
+++ chat/src/codec.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code)]
+// #![allow(dead_code)]
 use std::io;
 use serde_json as json;
 use byteorder::{BigEndian , ByteOrder};
@@ -80,45 +80,45 @@ impl Encoder for ChatCodec
 }
 
 
-/// Codec for Server -> Client transport
-pub struct ClientChatCodec;
+// Codec for Server -> Client transport
+// pub struct ClientChatCodec;
 
-impl Decoder for ClientChatCodec
-{
-    type Item = ChatResponse;
-    type Error = io::Error;
+// impl Decoder for ClientChatCodec
+// {
+//     type Item = ChatResponse;
+//     type Error = io::Error;
 
-    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
-        let size = {
-            if src.len() < 2 {
-                return Ok(None)
-            }
-            BigEndian::read_u16(src.as_ref()) as usize
-        };
+//     fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+//         let size = {
+//             if src.len() < 2 {
+//                 return Ok(None)
+//             }
+//             BigEndian::read_u16(src.as_ref()) as usize
+//         };
 
-        if src.len() >= size + 2 {
-            src.split_to(2);
-            let buf = src.split_to(size);
-            Ok(Some(json::from_slice::<ChatResponse>(&buf)?))
-        } else {
-            Ok(None)
-        }
-    }
-}
+//         if src.len() >= size + 2 {
+//             src.split_to(2);
+//             let buf = src.split_to(size);
+//             Ok(Some(json::from_slice::<ChatResponse>(&buf)?))
+//         } else {
+//             Ok(None)
+//         }
+//     }
+// }
 
-impl Encoder for ClientChatCodec
-{
-    type Item = ChatRequest;
-    type Error = io::Error;
+// impl Encoder for ClientChatCodec
+// {
+//     type Item = ChatRequest;
+//     type Error = io::Error;
 
-    fn encode(&mut self, msg: ChatRequest, dst: &mut BytesMut) -> Result<(), Self::Error> {
-        let msg = json::to_string(&msg).unwrap();
-        let msg_ref: &[u8] = msg.as_ref();
+//     fn encode(&mut self, msg: ChatRequest, dst: &mut BytesMut) -> Result<(), Self::Error> {
+//         let msg = json::to_string(&msg).unwrap();
+//         let msg_ref: &[u8] = msg.as_ref();
 
-        dst.reserve(msg_ref.len() + 2);
-        dst.put_u16::<BigEndian>(msg_ref.len() as u16);
-        dst.put(msg_ref);
+//         dst.reserve(msg_ref.len() + 2);
+//         dst.put_u16::<BigEndian>(msg_ref.len() as u16);
+//         dst.put(msg_ref);
 
-        Ok(())
-    }
-}
+//         Ok(())
+//     }
+// }

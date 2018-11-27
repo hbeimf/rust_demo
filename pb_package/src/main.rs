3df0479152fbb5907611805efbe749bcd1484e6a
maomao <5622876@qq.com>
2018-11-27 15:28:15 +0800

    encode && decode

--- pb_package/src/main.rs
+++ pb_package/src/main.rs
@@ -10,15 +10,21 @@
 
 extern crate protobuf;
 
-use protobuf::Message;
+// use protobuf::Message;
+use protobuf::*;
 mod protos;
 
 fn main() {
+	//encode
     let mut test_msg = protos::msg::TestMsg::new();
     test_msg.set_name("tom".to_owned());
     test_msg.set_nick_name("nick_name".to_owned());
     test_msg.set_phone("15912341234".to_owned());
 
     let serialized :Vec<u8> = test_msg.write_to_bytes().unwrap();
-    println!("{:?}", serialized);
+    println!("encode: {:?}", serialized);
+
+    // decode 
+    let parsed = parse_from_bytes::<protos::msg::TestMsg>(&serialized).unwrap();
+    println!("decode: {:?}", parsed);
 }

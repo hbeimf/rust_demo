ffce6f1ec2e5d7e07d873b65e5066b8bc294e6b9
maomao <5622876@qq.com>
2018-11-29 14:44:50 +0800

    pb

---
+++ chat/src/msg_proto.rs
@@ -0,0 +1,20 @@
+use protobuf::*;
+
+use super::protos;
+
+pub fn decode_msg(pb:Vec<u8>) -> protos::msg::TestMsg {
+    let test_msg : protos::msg::TestMsg = parse_from_bytes::<protos::msg::TestMsg>(&pb).unwrap();
+    // println!("decode: {:?}", parsed);
+    test_msg
+}
+
+pub fn encode_msg() -> Vec<u8> {
+    let mut test_msg = protos::msg::TestMsg::new();
+    test_msg.set_name("tom".to_owned());
+    test_msg.set_nick_name("nick_name".to_owned());
+    test_msg.set_phone("15912341234".to_owned());
+
+    let msg :Vec<u8> = test_msg.write_to_bytes().unwrap();
+    msg
+}
+

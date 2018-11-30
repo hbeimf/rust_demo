df23ac64814cca70d232defd0fe1da52c20bd5f7
maomao <5622876@qq.com>
2018-11-30 18:27:59 +0800

    make run

--- mysql_pool_demo/src/main.rs
+++ mysql_pool_demo/src/main.rs
@@ -1,9 +1,13 @@
+extern crate easy_logging;
+#[macro_use] extern crate log;
+// https://crates.io/crates/easy-logging
+
 extern crate mysql;
 extern crate r2d2_mysql;
 extern crate r2d2;
 // use std::env;
 use std::sync::Arc;
-use std::thread;
+// use std::thread;
 use mysql::{Opts,OptsBuilder};
 use r2d2_mysql::MysqlConnectionManager;
 
@@ -11,6 +15,12 @@ use r2d2_mysql::MysqlConnectionManager;
 // https://github.com/outersky/r2d2-mysql/blob/master/src/lib.rs
 
 fn main() {
+    // 初始化日志功能
+    easy_logging::init(module_path!(), log::Level::Debug).unwrap();
+    // easy_logging::init(module_path!(), log::Level::Info).unwrap();
+
+    debug!("mysql demo");
+
     // let db_url =  env::var("DATABASE_URL").unwrap();
     let db_url =  "mysql://root:123456@localhost:3306/xdb";
     let opts = Opts::from_url(&db_url).unwrap();
@@ -18,19 +28,21 @@ fn main() {
     let manager = MysqlConnectionManager::new(builder);
     let pool = Arc::new(r2d2::Pool::builder().max_size(4).build(manager).unwrap());
 
-    let mut tasks = vec![];
+    // let mut tasks = vec![];
 
-    for i in 0..3 {
-        let pool = pool.clone();
-        let th = thread::spawn(move || {
+    // for i in 0..3 {
+    //     let pool = pool.clone();
+    //     let th = thread::spawn(move || {
             let mut conn = pool.get().unwrap();
-            conn.query("select user()").unwrap();
-            println!("thread {} end!" , i );
-        });
-        tasks.push(th);
-    }
+            let r = conn.query("select * from users").unwrap();
+            debug!("reply: {:?}", r);
 
-    for th in tasks {
-        let _ = th.join();
-    }
+            // println!("thread {} end!" , i );
+    //     });
+    //     tasks.push(th);
+    // }
+
+    // for th in tasks {
+    //     let _ = th.join();
+    // }
 }

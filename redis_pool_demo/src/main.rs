35e9ef138f91de57d52cf4bf1c8bf4b503372e91
maomao <5622876@qq.com>
2018-11-30 16:35:46 +0800

    get set demo

--- redis_pool_demo/src/main.rs
+++ redis_pool_demo/src/main.rs
@@ -7,7 +7,7 @@ use std::ops::Deref;
 // use std::thread;
 
 use r2d2_redis::{r2d2, redis, RedisConnectionManager};
-use r2d2_redis::redis::Commands;
+use r2d2_redis::redis::{Commands};
 
 // https://docs.rs/redis/0.9.1/redis/
 fn main() {
@@ -22,18 +22,37 @@ fn main() {
         .build(manager)
         .unwrap();
 
-    for _i in 0..10i32 {
-        let conn = pool.get().unwrap();
-        // conn.set("key1", 123i32).unwrap();
-        let reply = redis::cmd("PING").query::<String>(conn.deref()).unwrap();
-        let _ : () = redis::cmd("SET").arg("my_key").arg(123).query(conn.deref()).unwrap();
-        let val : i32 = conn.get("my_key").unwrap();
+    // use ========================================
+    let conn = pool.get().unwrap();
 
-        debug!("reply:{}, val:{}", reply, val);
+    // ping 
+    let reply = redis::cmd("PING").query::<String>(conn.deref()).unwrap();	
+    debug!("ping reply:{}", reply);
 
-        let n: i64 = conn.incr("counter", 1).unwrap();
-        debug!("Counter increased to {}", n);
-    }
+    // set 
+    let _ : () = redis::cmd("SET").arg("my_key").arg(123).query(conn.deref()).unwrap();
+    let val : i32 = conn.get("my_key").unwrap();
+    debug!("set val:{}", val);
+
+    let _ = conn.set::<String, String, String>("key1".to_string(), "vvvvvvvvvv".to_string());
+    let val1: String = conn.get("key1").unwrap();
+    debug!("set val1: {}", val1);
+
+    let _ = conn.set::<String, i32, String>("key2".to_string(), 123456i32);
+    let val1: i32 = conn.get("key2").unwrap();
+    debug!("set val2: {}", val1);
+
+    
+    // hset 
+	let _ = conn.hset::<String, String, String, String>("hname".to_string(), "hkeyname".to_string(), "hval123456".to_string());
+    let hval: String = conn.hget("hname", "hkeyname").unwrap();
+    debug!("hset hval: {}", hval);
+
+   
+    // incr 
+    let n: i64 = conn.incr("counter", 1).unwrap();
+    debug!("Counter increased to {}", n);
+
   
 }
 

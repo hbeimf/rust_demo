231c8a1765d89bf1c31fc2a494d5cc9de0a0af42
maomao <5622876@qq.com>
2018-11-30 15:00:09 +0800

    make run

--- redis_pool_demo/src/main.rs
+++ redis_pool_demo/src/main.rs
@@ -1,3 +1,43 @@
+extern crate easy_logging;
+#[macro_use] extern crate log;
+// https://crates.io/crates/easy-logging
+
+extern crate r2d2_redis;
+
+
+use r2d2_redis::{r2d2, RedisConnectionManager};
+use r2d2_redis::redis::Commands;
+
+// use bincode::rustc_serialize::{encode, decode};
+// // use redis::Commands;
+// use r2d2_redis::redis::RedisResult;
+// use r2d2_redis::redis::ToRedisArgs;
+// use r2d2_redis::redis::FromRedisValue;
+// use r2d2_redis::redis::RedisError;
+// use r2d2_redis::redis::Value;
+
 fn main() {
-    println!("Hello, world!");
+	// 初始化日志功能
+    easy_logging::init(module_path!(), log::Level::Debug).unwrap();
+    // easy_logging::init(module_path!(), log::Level::Info).unwrap();
+
+    debug!("redis demo");
+
+    let manager = RedisConnectionManager::new("redis://localhost:6379").unwrap();
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .unwrap();
+
+
+	let conn = pool.get().unwrap();
+    let n: i64 = conn.incr("counter", 1).unwrap();
+    debug!("Counter increased to {}", n);
+
+
+    // conn.set("my_key", EncodeWrapper(42) );
+
+    let res: u64 = conn.get("counter").unwrap();
+    debug!("get {}", res);
+     
 }
+

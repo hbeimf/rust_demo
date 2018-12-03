da1ca49e99dfa622213ebf612120131f2d250712
maomao <5622876@qq.com>
2018-12-03 18:27:07 +0800

    mysql demo

---
+++ mysql_demo/src/lib.rs
@@ -0,0 +1,11 @@
+
+#[macro_use]
+extern crate diesel;
+// extern crate diesel_codegen;
+extern crate r2d2;
+extern crate r2d2_diesel;
+
+pub mod mysqlc;
+
+
+
---
+++ mysql_demo/src/main.rs
@@ -0,0 +1,5 @@
+extern crate mysql_demo;
+
+fn main() {
+	mysql_demo::mysqlc::test::test();
+}

c08c15f53b429a065138f934aad39a946dde63d7
maomao <5622876@qq.com>
2018-12-26 11:56:58 +0800

    rpc call

---
+++ test/tcp_client/apps/tcp_pool/src/tcp_rpc_call_table.erl
@@ -0,0 +1,29 @@
+% tcpc.erl
+-module(tcp_rpc_call_table).
+-compile(export_all).
+
+% -define(TIMEOUT, 5000).
+
+-define(ETS_OPTS,[set, public ,named_table , {keypos,2}, {heir,none}, {write_concurrency,true}, {read_concurrency,false}]).
+
+-define(RPC_CALL_DB, rpc_call_db).
+-record(rpc_call_db, {
+	key,
+	val
+}).
+
+% tcp_rpc_call_table:init_rpc_call_db().
+init_rpc_call_db() ->
+	ets:new(?RPC_CALL_DB, ?ETS_OPTS).
+
+insert(Key, Val) ->
+	ets:insert(?RPC_CALL_DB, #rpc_call_db{key=Key, val=Val}).
+
+select(Key) -> 
+	case ets:match_object(?RPC_CALL_DB, #rpc_call_db{key = Key,_='_'}) of
+		[{?RPC_CALL_DB, Key, Val}] -> {ok, Val};
+		[] ->{error,not_exist}
+	end.
+
+delete(Key) ->
+	ets:delete(?RPC_CALL_DB, Key).

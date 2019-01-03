2a3f8ba70895297e8f0943d8420e8b17c5321288
maomao <5622876@qq.com>
2019-01-03 16:13:19 +0800

    x

--- mysqlc/src/table_post.rs
+++ mysqlc/src/table_post.rs
@@ -9,10 +9,14 @@
 // extern crate diesel;
 
 use diesel::prelude::*;
-use schema::{posts};
-// use schema::*;
+// use schema::{posts};
+use schema::*;
+
+// use schema::posts::dsl::*;
+// use schema::posts::*;
 
 use diesel::*;
+// use pool;
 
 
 #[derive(Queryable)]
@@ -51,24 +55,63 @@ pub fn create_post(conn: &MysqlConnection, title: &str, body: &str) -> Post {
 
 
 // select 
+// https://github.com/diesel-rs/diesel/blob/2ce0e4ea0fda474459139042247512f0c8b254cf/diesel_tests/tests/raw_sql.rs
 pub fn select(connection: &MysqlConnection) {
     // use self::schema::posts::dsl::*;
-    use schema::posts::dsl::{posts, published};
+    // use schema::posts::dsl::{posts, published};
 
-    let results = posts
-        .filter(published.eq(false))
-        // .limit(5)
-        .load::<Post>(connection)
-        .expect("Error loading posts");
+    // let results = posts
+    //     .filter(published.eq(false))
+    //     // .limit(5)
+    //     .load::<Post>(connection)
+    //     .expect("Error loading posts");
 
-    println!("Displaying {} posts", results.len());
-    for post in results {
-        println!("{}", post.id);
+    // println!("Displaying {} posts", results.len());
+    // for post in results {
+    //     println!("{}", post.id);
 
-        println!("{}", post.title);
-        println!("{}", post.body);
-        println!("-----------\n");
-        break;
-    }
+    //     println!("{}", post.title);
+    //     println!("{}", post.body);
+    //     println!("-----------\n");
+    //     break;
+    // }
+
+     let rows = sql_query("SELECT id FROM posts ORDER BY id").execute(connection); 
+     println!("{:?}", rows);
+     
+     // let rows = sql_query("SELECT * FROM posts").load(connection); 
+     // println!("{:?}", rows);
+
 }
 
+
+// query 
+
+// pub fn query() {
+
+//     let mysql = pool::MYSQL_INSTANCE.get(); 
+//     // let conn = redis.pool.get().unwrap();
+    
+//     match mysql.pool.get() {
+//                 Ok(conn) => {
+//                         // insert 
+//                         // table_post::create_post(&conn, "titletest", "body test");
+//                         // table_post::select(&conn);
+//                         let rows = sql_query("SELECT id FROM posts ORDER BY id").load(&conn); 
+//                         // println!("rows:{:?}", rows);
+
+//                 },
+//                 // Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
+//                 _ => {
+//                         println!("something else");
+//                 }
+//     }   
+
+//     // let users = sql_query("SELECT * FROM users ORDER BY id")
+//     //     .load(&connection);
+//     // let expected_users = vec![
+//     //     User { id: 1, name: "Sean".into() },
+//     //     User { id: 2, name: "Tess".into() },
+//     // ];
+//     // assert_eq!(Ok(expected_users), users);
+// }

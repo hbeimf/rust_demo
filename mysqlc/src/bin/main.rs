482e7d24278b6453010347b8d7ee38cba509f88f
maomao <5622876@qq.com>
2019-01-07 16:26:53 +0800

    make run

---
+++ mysqlc/src/bin/main.rs
@@ -0,0 +1,13 @@
+extern crate mysqlc;
+use mysqlc::*;
+
+extern crate easy_logging;
+
+fn main() {
+	 // 初始化日志功能
+    easy_logging::init(module_path!(), log::Level::Debug).unwrap();
+    // easy_logging::init(module_path!(), log::Level::Info).unwrap();
+
+    // debug!("Hello, world!");
+    test::test();
+}
---
+++ mysqlc/src/lib.rs
@@ -0,0 +1,21 @@
+#[macro_use]
+extern crate diesel;
+// extern crate diesel_codegen;
+extern crate r2d2;
+extern crate r2d2_diesel;
+
+// pub mod mysqlc;
+// extern crate sys_config;
+
+// https://github.com/crlf0710/singleton-rs/blob/master/src/bin/trial.rs
+#[macro_use]
+extern crate singleton;
+// use singleton::{Singleton};
+
+pub mod test;
+pub mod pool;
+pub mod schema;
+pub mod table_posts;
+
+extern crate easy_logging;
+#[macro_use] extern crate log;

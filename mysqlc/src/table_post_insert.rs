2150898690118b069a0d2ff6ba9b1060ae926b85
maomao <5622876@qq.com>
2019-01-07 11:01:07 +0800

    insert

--- mysqlc/src/table_post_insert.rs
+++ mysqlc/src/table_post_insert.rs
@@ -6,19 +6,9 @@
 //   PRIMARY KEY (`id`)
 // ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='test';
 
-// extern crate diesel;
 
 use diesel::prelude::*;
 use schema::posts;
-
-// #[derive(Queryable)]
-// pub struct Post {
-//     pub id: i32,
-//     pub title: String,
-//     pub body: String,
-//     pub published: bool,
-// }
-
 
 pub use table_post_select::Post;
 
@@ -48,27 +38,4 @@ pub fn create_post(conn: &MysqlConnection, title: &str, body: &str) -> Post {
 
     posts.order(id.desc()).first(conn).unwrap()
 }
-
-
-// // select 
-// pub fn select(connection: &MysqlConnection) {
-//     // use self::schema::posts::dsl::*;
-//     use schema::posts::dsl::{posts, published};
-
-//     let results = posts
-//         .filter(published.eq(false))
-//         // .limit(5)
-//         .load::<Post>(connection)
-//         .expect("Error loading posts");
-
-//     println!("Displaying {} posts", results.len());
-//     for post in results {
-//         println!("{}", post.id);
-
-//         println!("{}", post.title);
-//         println!("{}", post.body);
-//         println!("-----------\n");
-//         break;
-//     }
-// }
 

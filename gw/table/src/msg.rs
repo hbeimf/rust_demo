7199defffb4b6e5116d5fd94ce196d354e24dbc5
maomao <5622876@qq.com>
2019-01-09 18:20:56 +0800

    return

--- gw/table/src/msg.rs
+++ gw/table/src/msg.rs
@@ -3,11 +3,19 @@ use actix::prelude::*;
 #[derive(Message, Debug)]
 pub struct TableMessage(pub Vec<u8>);
 
-#[derive(Message)]
-#[rtype(u32)]
+// #[derive(Message)]
+// #[rtype(u32)]
 pub struct Connect {
     pub uid: u32,
     pub addr: Recipient<TableMessage>,
+}
+
+// 简单返回类型
+// #[derive(Message)]
+// #[rtype(u32)]
+// 复杂返回类型手动实现 actix::Message
+impl actix::Message for Connect {
+    type Result = u32;
 }
 
 // handler_cast()

c94bb711107914992d8a97646fea5ce3ce48c76b
maomao <5622876@qq.com>
2019-01-09 11:42:17 +0800

    make && make run

--- gw/gateway/src/main.rs
+++ gw/gateway/src/main.rs
@@ -13,14 +13,19 @@ extern crate flexi_logger;
 extern crate log;
 use flexi_logger::{Logger, detailed_format};
 
+use std::path::Path;
 
-/// gw-server 测试服
+/// rs-server 试验服
 #[derive(StructOpt, Debug)]
-#[structopt(name = "mysqlc")]
+#[structopt(name = "rs-server")]
 struct Opt {
     /// 配置文件路径，例如: /etc/gw_server_config.ini
     #[structopt(short = "c", long = "config", parse(from_os_str))]
     config: Vec<PathBuf>,
+
+    /// 日志存放目录，例如: /erlang/rust_demo/
+    #[structopt(short = "d", long = "dir")]
+    dir: Vec<String>,
 
     /// 日志等级，例如: error|warn|info|debug|trace
     #[structopt(short = "l", long = "level")]
@@ -31,10 +36,48 @@ fn main() {
     let opt = Opt::from_args();
     println!("{:?}", opt);
     
-	Logger::with_str("debug")
+    // config
+    let mut config: PathBuf = PathBuf::from(r"/erlang/rust_demo/gw/default_config.ini");
+    if ! opt.config.is_empty() {
+        if let Some(conf) = opt.config.get(0) {
+            if conf.is_file() {
+                config = (*conf).clone();
+            }
+        }
+    }
+
+    if let Some(config_dir) = config.to_str(){
+        let con: String = config_dir.to_string();
+        println!("config_dir :{:?}", con);
+    }
+
+    // log dir 
+    let mut log_dir: String = "/erlang/rust_demo/gw/logs/".to_string();
+    if ! opt.dir.is_empty() {
+        if let Some(dir) = opt.dir.get(0) {
+            if Path::new(dir).is_dir() {
+                log_dir = (*dir).clone();
+            }
+        }
+    }
+    println!("log_dir: {:?}", log_dir);
+    // log level
+    let mut log_level: String = "debug".to_string();
+    if ! opt.level.is_empty() {
+        if let Some(level) = opt.dir.get(0) {
+            let levels = ["error", "warn", "info", "debug", "trace"];
+            if levels.contains(&level.as_str()) {
+                log_level = (*level).clone();
+            }
+        }
+    }
+    println!("log_level: {:?}", log_level);
+
+
+	Logger::with_str(log_level)
         .format(detailed_format)
         .log_to_file()
-        .directory("logs")
+        .directory(log_dir)
         .rotate_over_size(200000000000)
         .o_timestamp(true)
         .start_reconfigurable()
@@ -47,8 +90,8 @@ fn main() {
     // info!("This is an info message");
     // trace!("This is a trace message - you must not see it!");
 
-    mysqlc::test::test();
-    redisc::test();
+    // mysqlc::test::test();
+    // redisc::test();
 
     let sys = actix::System::new("websocket-example");
     tcp_server::start_server();

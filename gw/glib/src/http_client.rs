2e58f13f5186b8424debd30458d46622a68422e3
maomao <5622876@qq.com>
2019-01-21 18:24:41 +0800

    dns

--- gw/glib/src/http_client.rs
+++ gw/glib/src/http_client.rs
@@ -2,16 +2,24 @@ use curl::http::handle;
 // use super::server;
 
 use encoding::{Encoding, DecoderTrap};
-use encoding::all::UTF_8;
+use encoding::all::{UTF_8, GBK};
 
 
+
+
+pub fn get(){
+	get_baidu();
+	get_test();
+}
 
 
 // https://github.com/alexcrichton/curl-rust/blob/0.2.18/test/server.rs
-pub fn get() {
+//dns
+//https://www.cnblogs.com/yudar/p/4723992.html
+pub fn get_test() {
 //	let url = "http://quotes.money.163.com/service/chddata.html?code=0900919&start=20000101&end=20190121".to_string();
-//	let url = "http://quotes.money.163.com/service/chddata.html?code=0900919&start=20190101&end=20190121".to_string();
-	let url = "https://www.baidu.com".to_string();
+	let url = "http://quotes.money.163.com/service/chddata.html?code=0900919&start=20190101&end=20190121".to_string();
+//	let url = "https://www.baidu.com".to_string();
 
 
 	let res = handle()
@@ -35,8 +43,44 @@ pub fn get() {
 //	println!("{:?}", body);
 
 
+	let res = GBK.decode(res.get_body(), DecoderTrap::Strict);
+	println!("res:{:?}", res);
+
+
+}
+
+
+
+pub fn get_baidu() {
+//	let url = "http://quotes.money.163.com/service/chddata.html?code=0900919&start=20000101&end=20190121".to_string();
+//	let url = "http://quotes.money.163.com/service/chddata.html?code=0900919&start=20190101&end=20190121".to_string();
+	let url = "https://www.baidu.com".to_string();
+
+
+	let res = handle()
+		.get(url)
+		.exec().unwrap();
+
+//	let res = handle()
+//		.get("https://www.baidu.com".to_string())
+//		.exec().unwrap();
+
+
+	// srv.assert();
+
+	// assert!(res.get_code() == 200, "code is {}", res.get_code());
+	// assert!(res.get_body() == "Hello".as_bytes());
+	// assert!(res.get_headers().len() == 1);
+	// assert!(res.get_header("content-length") == ["5".to_string()]);
+//	 println!("{:?}", res.get_body());
+
+//	let body = String::from_utf8(res.get_body().to_vec());
+//	println!("{:?}", body);
+
+
 	let res = UTF_8.decode(res.get_body(), DecoderTrap::Strict);
 	println!("res:{:?}", res);
 
 
 }
+

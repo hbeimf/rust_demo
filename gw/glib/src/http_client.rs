96d84adcd47897dd02c2dd3e788387b44869423c
maomao <5622876@qq.com>
2019-01-21 14:20:09 +0800

    gbk

--- gw/glib/src/http_client.rs
+++ gw/glib/src/http_client.rs
@@ -2,16 +2,23 @@ use curl::http::handle;
 // use super::server;
 
 use encoding::{Encoding, DecoderTrap};
-use encoding::all::UTF_8;
+use encoding::all::GBK;
 
 
 
 
 // https://github.com/alexcrichton/curl-rust/blob/0.2.18/test/server.rs
 pub fn get() {
+	let url = "http://quotes.money.163.com/service/chddata.html?code=0900919&start=20000101&end=20190121".to_string();
+
 	let res = handle()
-	.get("https://www.baidu.com".to_string())
+	.get(url)
 	.exec().unwrap();
+
+//	let res = handle()
+//		.get("https://www.baidu.com".to_string())
+//		.exec().unwrap();
+
 
 	// srv.assert();
 
@@ -25,7 +32,7 @@ pub fn get() {
 //	println!("{:?}", body);
 
 
-	let res = UTF_8.decode(res.get_body(), DecoderTrap::Strict);
+	let res = GBK.decode(res.get_body(), DecoderTrap::Strict);
 	println!("res:{:?}", res);
 
 

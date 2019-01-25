c51b93798fe350fa7bd64da2c7a357e0dc049c79
maomao <5622876@qq.com>
2019-01-25 17:24:13 +0800

    x

--- gw/glib/src/aes.rs
+++ gw/glib/src/aes.rs
@@ -84,12 +84,11 @@ pub fn encode(from_v8:Vec<u8>) -> Vec<u8> {
 }
 
 fn key() -> Vec<u8> {
-    let key = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F";
-    key.to_vec()
+    let key = "201707eggplant99".to_string();
+    key.into_bytes()
 }
 
 fn iv() -> Vec<u8> {
-    let iv = *b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\
-                \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
-    iv.to_vec()
+    let iv = "12345678901234561234567890123456".to_string();
+    iv.into_bytes()
 }

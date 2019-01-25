75d22791dcaa2e8dfdcee0f12fd17f8b530ce9d5
maomao <5622876@qq.com>
2019-01-25 14:12:12 +0800

    aes key

--- gw/glib/src/aes.rs
+++ gw/glib/src/aes.rs
@@ -9,33 +9,45 @@ pub fn test() {
     let plaintext = b"\x12\x34\x56\x78\x90\x12\x34\x56\x12\x34\x56\x78\x90\x12\x34\x56";
 //    dbg!(plaintext);
 
-    let en = encode(plaintext);
+    let en = encode(plaintext.to_vec());
     let de = decode(en);
 //    dbg!(de);
-    assert_eq!(*plaintext, de);
+    assert_eq!(plaintext.to_vec(), de);
 }
 
-pub fn decode(en: [u8; 16]) -> [u8; 16] {
-    let key = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F";
-    let mut iv = *b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\
-                \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
+pub fn decode(en: Vec<u8>) -> Vec<u8> {
+//    let key = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F";
+//    let mut iv = *b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\
+//                \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
 
-    let key = AesKey::new_decrypt(key).unwrap();
+    let key = key();
+    let mut iv = iv();
+
+    let key = AesKey::new_decrypt(&key).unwrap();
     let mut output = [0u8; 16];
     aes_ige(&en, &mut output, &key, &mut iv, Mode::Decrypt);
-    output
+    output.to_vec()
 
 }
 
-pub fn encode(plaintext:&[u8; 16]) -> [u8; 16] {
+pub fn encode(plaintext:Vec<u8>) -> Vec<u8> {
 
+    let key = key();
+    let mut iv = iv();
 
-    let key = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F";
-    let mut iv = *b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\
-                \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
-
-    let key = AesKey::new_encrypt(key).unwrap();
+    let key = AesKey::new_encrypt(&key).unwrap();
     let mut output = [0u8; 16];
-    aes_ige(plaintext, &mut output, &key, &mut iv, Mode::Encrypt);
-    output
+    aes_ige(&plaintext, &mut output, &key, &mut iv, Mode::Encrypt);
+    output.to_vec()
+}
+
+fn key() -> Vec<u8> {
+    let key = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F";
+    key.to_vec()
+}
+
+fn iv() -> Vec<u8> {
+    let iv = *b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\
+                \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
+    iv.to_vec()
 }

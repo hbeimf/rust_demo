1e2e31fb7b35a6b76f35282840e6bc4e43b8d247
maomao <5622876@qq.com>
2019-01-25 14:28:53 +0800

    x

--- gw/glib/src/aes.rs
+++ gw/glib/src/aes.rs
@@ -4,21 +4,26 @@ use crate::openssl::aes::{AesKey, aes_ige};
 use crate::openssl::symm::Mode;
 
 //https://docs.rs/openssl/0.10.16/openssl/aes/index.html
-pub fn test() {
-
+pub fn test1() {
     let plaintext = b"\x12\x34\x56\x78\x90\x12\x34\x56\x12\x34\x56\x78\x90\x12\x34\x56";
-//    dbg!(plaintext);
-
     let en = encode(plaintext.to_vec());
     let de = decode(en);
-//    dbg!(de);
     assert_eq!(plaintext.to_vec(), de);
 }
 
+pub fn test() {
+//    let plaintext = b"\x12\x34\x56\x78\x90\x12\x34\x56\x12\x34\x56\x78\x90\x12\x34\x56";
+    let plaintext = vec![1,2,3,4,5,6,7,8,9,0,11,12,13,14,15, 16];
+    let en = encode(plaintext.clone());
+    let de = decode(en);
+//    assert_eq!(plaintext, de);
+
+    dbg!(plaintext);
+    dbg!(de);
+}
+
+
 pub fn decode(en: Vec<u8>) -> Vec<u8> {
-//    let key = b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F";
-//    let mut iv = *b"\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F\
-//                \x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F";
 
     let key = key();
     let mut iv = iv();

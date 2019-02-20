c76bca5dc379c6df136fe9202a47f28b024929d7
maomao <5622876@qq.com>
2019-02-20 15:45:57 +0800

    msg

--- erust/apps/rs/src/rs.erl
+++ erust/apps/rs/src/rs.erl
@@ -2,10 +2,33 @@
 -module(rs).
 -compile(export_all).
 
+-define(TIMEOUT, 5000).
+
+-include("msg_proto.hrl").
+-include_lib("glib/include/log.hrl").
+% -include_lib("glib/include/cmdid.hrl").
+
+
 test() -> 
-	ok.
+	Package = <<"hello world">>,
+	call(Package).
 	
+call(Package) ->
+	Key = glib:to_binary(glib:to_str(glib:uid())),	
+	?LOG({key, Key}),
+	% tcp_rpc_call_table:insert(Key, self()),
 
+	RpcPackage = #'RpcPackage'{
+                        key = Key,
+                        payload = Package
+                    },
+	RpcPackageBin = msg_proto:encode_msg(RpcPackage),
 
+	RpcPackageBin1 = glib:package(10008, RpcPackageBin),
 
+	poolboy:transaction(pool_name(), fun(Worker) ->
+		gen_server:call(Worker, {call, Key, RpcPackageBin1}, ?TIMEOUT)
+	end).
 
+pool_name() ->
+	rs_client_pool.

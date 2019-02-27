04cf52afebc0f97786d0ed246fd3bfd792e73eaf
maomao <5622876@qq.com>
2019-02-27 10:40:11 +0800

    cast

--- erust/apps/rs/include/cmd.hrl
+++ erust/apps/rs/include/cmd.hrl
@@ -2,6 +2,6 @@
 -define(CMD_ID,true).
 
 -define(CMD_CALL_10008, 10008).  %% call
-% -define(CMD_CAST_10010, 10010).  %% cast
+-define(CMD_CAST_10010, 10010).  %% cast
 
 -endif.
--- erust/apps/rs/src/rs.erl
+++ erust/apps/rs/src/rs.erl
@@ -8,12 +8,20 @@
 -include_lib("glib/include/log.hrl").
 
 test() -> 
+	test_call(),
+	test_cast().
+
+test_call() -> 
 	Package = <<"hello world">>,
 	RpcReply = call(Package),
 	?LOG(RpcReply),
 	#'RpcPackage'{key = Key, 'payload' = Payload} = msg_proto:decode_msg(RpcReply,'RpcPackage'),
 	?LOG(Payload),
 	ok.
+
+test_cast() -> 
+	Package = <<"hello world">>,
+	RpcReply = cast(Package).	
 	
 call(Package) ->
 	Key = glib:to_binary(glib:to_str(glib:uid())),	
@@ -26,6 +34,21 @@ call(Package) ->
 	poolboy:transaction(pool_name(), fun(Worker) ->
 		gen_server:call(Worker, {call, Key, RpcPackageBin1}, ?TIMEOUT)
 	end).
+
+
+
+cast(Package) ->
+	Key = glib:to_binary(glib:to_str(glib:uid())),	
+	RpcPackage = #'RpcPackage'{
+                        key = Key,
+                        payload = Package
+                    },
+	RpcPackageBin = msg_proto:encode_msg(RpcPackage),
+	RpcPackageBin1 = glib:package(?CMD_CAST_10010, RpcPackageBin),
+	poolboy:transaction(pool_name(), fun(Worker) ->
+		gen_server:cast(Worker, {send, RpcPackageBin1})
+	end).
+
 
 pool_name() ->
 	rs_client_pool.

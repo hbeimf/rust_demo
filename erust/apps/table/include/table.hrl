177c81e927ea39f35adaad461e0fc74e3651854a
maomao <5622876@qq.com>
2019-03-04 17:57:38 +0800

    t

---
+++ erust/apps/table/include/table.hrl
@@ -0,0 +1,5 @@
+
+-record(codes, {
+	name,
+	code
+}).
---
+++ erust/apps/table/src/table_create.erl
@@ -0,0 +1,24 @@
+-module(table_create).
+-compile(export_all).
+
+-include_lib("stdlib/include/qlc.hrl").
+%% 定义记录结构
+
+-include("table.hrl").
+% -include_lib("ws_server/include/log.hrl").
+
+-define(WAIT_FOR_TABLES, 10000).
+
+%% 初始化mnesia表结构
+init() ->
+	case mnesia:create_schema([node()]) of
+		ok -> 
+			    mnesia:start(),
+			    mnesia:create_table(codes, [{attributes,record_info(fields,codes)}]),
+			    ok;
+	  _ -> 
+	  	mnesia:start()
+	 end.
+
+
+

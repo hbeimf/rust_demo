b774fcef35a191c56660b321d1e0e714a26d3623
maomao <5622876@qq.com>
2019-03-26 15:48:11 +0800

    x

--- erust/apps/eredis_cluster/src/redis.erl
+++ erust/apps/eredis_cluster/src/redis.erl
@@ -1,14 +1,58 @@
 -module(redis).
 -compile(export_all).
 
-% -export([test/0]).
+-define(LOG2(X), io:format("~n==========log2========{~p,~p}==============~n~p~n", [?MODULE,?LINE,X])).
+
 
 test() -> 
-	eredis_cluster:q(["GET","foo"]).
+  ?LOG2({test_update, "v0.1.2"}),
+	set(),
+	redisc_get().
+
+redisc_get() ->
+    redisc_get("foo").
+redisc_get(Key) ->
+    q(["GET", Key]).
+
+set() ->
+    set("foo", "bar").
+set(Key, Val) ->
+    q(["SET", Key, Val]).
+
+get() ->
+    redis:get("foo").
+get(Key) ->
+    q(["GET", Key]).
+    
+hget() -> 
+    Hash = "info@341659",
+    A = hget(Hash, "gold"),
+    B = hgetall(Hash),
+    {A, B}.
+
+hget(Hash, Key) -> 
+    q(["hget", Hash, Key]).
+
+hgetall(Hash) -> 
+    q(["hgetall", Hash]).
+    
+hset(Hash, Key, Val) ->
+    q(["hset", Hash, Key, Val]). 
+
+incr() ->
+  incr("test_incr").
+incr(Key) ->
+  q(["incr", Key]).
+
+decr() ->
+  decr("test_incr").
+decr(Key) ->
+  q(["decr", Key]).
 
 
 
-
+q(Command) -> 
+    eredis_cluster:q(Command).
 
 
 

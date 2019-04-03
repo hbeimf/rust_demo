1a514d7c358e47f28aeea72d9cb4e5068d1d3697
maomao <5622876@qq.com>
2019-04-03 15:14:21 +0800

    l

--- erust/apps/glib/src/glog.erl
+++ erust/apps/glib/src/glog.erl
@@ -5,22 +5,38 @@
 -include_lib("glib/include/log.hrl").
 
 test() ->
-	{ok, Log} = log:open("log_test"),
-	% {ok, {A, _}} = util:count(fun (I, _) -> log:write(Log, util:bin(I)) end, {ok, undefined}, 1000),
-	W = log:write(Log, term_to_binary({hello})),
-	R = log:since(Log, undefined),
+	Dir = "log_test",
+	W = write(Dir, {hello}),
+	LogPid = log_pid(Dir),
+	R = log:since(LogPid, undefined),
 	?LOG(W),
 	?LOG(R),
 
 	{ok, {Start, _}} = W,
-	{_, {ok, Bin}} = log:fetch(Log, Start),
+	{_, {ok, Bin}} = log:fetch(LogPid, Start),
 	?LOG(binary_to_term(Bin)),
 
-	Acc = log:foldl(Log, fun(Node, Reply) -> 
-		?LOG(Node),
+	foreach_log(Dir),
+	ok.
+
+
+foreach_log(Dir) ->
+	LogPid = log_pid(Dir),
+	Acc = log:foldl(LogPid, fun(EachLog, Reply) -> 
+		?LOG(EachLog),
 		Reply
 	end, []),
 	?LOG(Acc),
-
 	ok.
+
+log_pid(Dir) ->
+	glib_sup:start_log(Dir).
+
+write(Dir, TermLog) -> 
+	Pid = glib_sup:start_log(Dir),
+	log:write(Pid, term_to_binary(TermLog)).
+
+
+
+
 

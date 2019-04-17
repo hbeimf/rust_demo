e85c903cb49ab5fa5a65c1d4aeeb115e569292c0
maomao <5622876@qq.com>
2019-04-17 17:59:47 +0800

    pu

--- erust/apps/glib/src/glog.erl
+++ erust/apps/glib/src/glog.erl
@@ -11,19 +11,19 @@ tt() ->
 	end, lists:seq(1,1000)),
 	ok.
 
-test() ->
-	Dir = "log_test",
-	lists:foreach(fun(I) -> 
-		W = write(Dir, {hello, world,  <<"log bin">>, [type, category]}),
-		% ?LOG(W),
-		ok
-	end, [1]),
+test_write() ->
+	Dir = dir(),
+	R = lists:foldl(fun(I, Reply) -> 
+		W = write(Dir, {I, hello, world}),
+		[{I, W}|Reply]
+	end, [], lists:seq(1,20)),
+	?LOG(R),
 	ok.
 	
 
 % // read 
-test1() ->
-	Dir = "log_test",
+test_read() ->
+	Dir = dir(),
 	% LogPid = log_pid(Dir),
 	% R = log:since(LogPid, undefined),
 	% ?LOG(W),
@@ -59,6 +59,44 @@ write(Dir, TermLog) ->
 	log:write(Pid, term_to_binary(Log)).
 
 
+range() ->
+	Dir = dir(),
+	range(Dir, {<<"00/00">>,131}, {<<"00/00">>,1733}).
+range(Dir, Start, End) ->
+	 Pid = log_pid(Dir),	
+	 log:range(Pid, {Start, End}).
 
 
+limit() ->
+	Dir = dir(),
+	limit(Dir, {<<"00/00">>,131}, {<<"00/00">>,1733}).
+limit(Dir, Start, End) ->
+	Pid = log_pid(Dir),	
+	Range = {Start, End},
+	log:limit(Pid, fun(D, Reply) -> 
+		{_, {ok, Bin}} = D,
+		?LOG(binary_to_term(Bin)),
+		Reply
+	end, [], Range, []),
+	ok.
+
+
+% [{20,{ok,{{<<"00/00">>,1733},{<<"00/00">>,1822}}}},
+% ...
+%  {2,{ok,{{<<"00/00">>,131},{<<"00/00">>,220}}}},
+%  {1,{ok,{{<<"00/00">>,42},{<<"00/00">>,131}}}}]
+
+fetch()->
+	Dir = dir(),
+	{_, {ok, Bin}} = fetch(Dir, {<<"00/00">>,1733}),
+	?LOG(binary_to_term(Bin)),
+	ok.
+fetch(Dir, Position) ->
+	Pid = log_pid(Dir),	
+	log:fetch(Pid, Position).
+
+
+dir() -> 
+	Dir = "log_test",
+	Dir.	
 

b80abcd7ca7e5cce8648863faead42821c322ec4
maomao <5622876@qq.com>
2019-07-09 18:12:14 +0800

    pu

---
+++ ego/apps/go/go_src/tcp_server/length_value_codec.go
@@ -0,0 +1,108 @@
+package tcp_server
+
+// length_value_codec.go
+
+import (
+	"bytes"
+	// "context"
+	"encoding/binary"
+	// "fmt"
+	"io"
+	"net"
+
+	"github.com/leesper/holmes"
+	"github.com/leesper/tao"
+)
+
+// const (
+// 	// MessageTypeBytes is the length of type header.
+// 	MessageTypeBytes = 4
+// 	// // MessageLenBytes is the length of length header.
+// 	// MessageLenBytes = 4
+// 	// // MessageMaxBytes is the maximum bytes allowed for application data.
+// 	// MessageMaxBytes = 1 << 23 // 8M
+// )
+
+// TypeLengthValueCodec defines a special codec.
+// Format: type-length-value |4 bytes|4 bytes|n bytes <= 8M|
+type TypeLengthValueCodec struct{}
+
+// Decode decodes the bytes data into Message
+func (codec TypeLengthValueCodec) Decode(raw net.Conn) (tao.Message, error) {
+	byteChan := make(chan []byte)
+	errorChan := make(chan error)
+
+	go func(bc chan []byte, ec chan error) {
+		typeData := make([]byte, tao.MessageTypeBytes)
+		_, err := io.ReadFull(raw, typeData)
+		if err != nil {
+			ec <- err
+			close(bc)
+			close(ec)
+			holmes.Debugln("go-routine read message type exited")
+			return
+		}
+		bc <- typeData
+	}(byteChan, errorChan)
+
+	var typeBytes []byte
+
+	select {
+	case err := <-errorChan:
+		return nil, err
+
+	case typeBytes = <-byteChan:
+		if typeBytes == nil {
+			holmes.Warnln("read type bytes nil")
+			return nil, tao.ErrBadData
+		}
+		typeBuf := bytes.NewReader(typeBytes)
+		var msgType int32
+		if err := binary.Read(typeBuf, binary.LittleEndian, &msgType); err != nil {
+			return nil, err
+		}
+
+		lengthBytes := make([]byte, tao.MessageLenBytes)
+		_, err := io.ReadFull(raw, lengthBytes)
+		if err != nil {
+			return nil, err
+		}
+		lengthBuf := bytes.NewReader(lengthBytes)
+		var msgLen uint32
+		if err = binary.Read(lengthBuf, binary.LittleEndian, &msgLen); err != nil {
+			return nil, err
+		}
+		if msgLen > tao.MessageMaxBytes {
+			holmes.Errorf("message(type %d) has bytes(%d) beyond max %d\n", msgType, msgLen, tao.MessageMaxBytes)
+			return nil, tao.ErrBadData
+		}
+
+		// read application data
+		msgBytes := make([]byte, msgLen)
+		_, err = io.ReadFull(raw, msgBytes)
+		if err != nil {
+			return nil, err
+		}
+
+		// deserialize message from bytes
+		unmarshaler := tao.GetUnmarshalFunc(msgType)
+		if unmarshaler == nil {
+			return nil, tao.ErrUndefined(msgType)
+		}
+		return unmarshaler(msgBytes)
+	}
+}
+
+// Encode encodes the message into bytes data.
+func (codec TypeLengthValueCodec) Encode(msg Message) ([]byte, error) {
+	data, err := msg.Serialize()
+	if err != nil {
+		return nil, err
+	}
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, msg.MessageNumber())
+	binary.Write(buf, binary.LittleEndian, int32(len(data)))
+	buf.Write(data)
+	packet := buf.Bytes()
+	return packet, nil
+}

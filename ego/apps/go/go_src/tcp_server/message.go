85058b1c6a9218b22a1d15777a0173f3fd131df4
maomao <5622876@qq.com>
2019-07-10 13:26:39 +0800

    pu

--- ego/apps/go/go_src/tcp_server/message.go
+++ ego/apps/go/go_src/tcp_server/message.go
@@ -62,6 +62,7 @@ func ProcessMessage(ctx context.Context, conn tao.WriteCloser) {
 
 	msg := tao.MessageFromContext(ctx).(Message)
 
+	log.Printf("receving message 62: %#v\n", msg)
 	log.Printf("receving message 62: %#v\n", msg.proto)
 
 	// 进行解码
@@ -75,6 +76,23 @@ func ProcessMessage(ctx context.Context, conn tao.WriteCloser) {
 	// log.Fatalf("data mismatch %q != %q", test.GetLabel(), newTest.GetLabel())
 	log.Printf("decode package Key 73: %#v", newAesEncode.GetKey())
 	log.Printf("decode package From 73: %#v", newAesEncode.GetFrom())
+
+	//
+	// 创建一个消息 Test
+	test := &glib.AesEncode{
+		// 使用辅助函数设置域的值
+		Key:  "123456",
+		From: "hello world",
+		// Type:  proto.Int32(17),
+	}
+
+	// 进行编码
+	data, err := proto.Marshal(test)
+	if err != nil {
+		// log.Fatal("marshaling error: ", err)
+		log.Printf("encode 93: %#v", err)
+	}
+	log.Printf("encode 93: %#v", data)
 
 	// return unmarshaler(msgBytes)
 	// return nil, nil

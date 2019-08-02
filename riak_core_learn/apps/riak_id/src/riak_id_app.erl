7447cdbca9b5cbc46fbcf130b6509c8b18117e78
mm <mm@mail.com>
2019-08-02 16:53:14 +0800

    x

--- riak_core_learn/apps/riak_id/src/riak_id_app.erl
+++ riak_core_learn/apps/riak_id/src/riak_id_app.erl
@@ -1,26 +1,23 @@
-%%%-------------------------------------------------------------------
-%% @doc riak_id public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(riak_id_app).
-
 -behaviour(application).
 
 %% Application callbacks
 -export([start/2, stop/1]).
 
-%%====================================================================
-%% API
-%%====================================================================
-
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
 start(_StartType, _StartArgs) ->
-    riak_id_sup:start_link().
+    case riak_id_sup:start_link() of
+        {ok, Pid} ->
+            ok = riak_core:register(riak_id, [{vnode_module, riak_id_vnode}]),
+            ok = riak_core_ring_events:add_guarded_handler(riak_id_ring_event_handler, []),
+            ok = riak_core_node_watcher_events:add_guarded_handler(riak_id_node_event_handler, []),
+            ok = riak_core_node_watcher:service_up(riak_id, self()),
+            {ok, Pid};
+        {error, Reason} ->
+            {error, Reason}
+    end.
 
-%%--------------------------------------------------------------------
 stop(_State) ->
     ok.
-
-%%====================================================================
-%% Internal functions
-%%====================================================================

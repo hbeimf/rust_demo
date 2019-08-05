6d7ff3313284286556d84cdc51f998ca167e1329
mm <mm@mail.com>
2019-08-05 14:48:23 +0800

    pu

--- riak_learn/apps/rc_example/src/rc_example.erl
+++ riak_learn/apps/rc_example/src/rc_example.erl
@@ -13,9 +13,27 @@
          clear/0
         ]).
 
-%% @doc Pings a random vnode to make sure communication is functional
-ping()->
-  ping(os:timestamp()).
+
+% rc_example:ping().
+ping() ->
+    % argument to chash_key has to be a two item tuple, since it comes from riak
+    % and the full key has a bucket, we use a contant in the bucket position
+    % and a timestamp as key so we hit different vnodes on each call
+    DocIdx = riak_core_util:chash_key({<<"ping">>, term_to_binary(os:timestamp())}),
+
+    % ?LOG(DocIdx),
+    % ask for 1 vnode index to send this request to, change N to get more
+    % vnodes, for example for replication
+    N = 1,
+    PrefList = riak_core_apl:get_primary_apl(DocIdx, N, rc_example),
+    [{IndexNode, _Type}] = PrefList,
+    riak_core_vnode_master:sync_spawn_command(IndexNode, ping, rc_example_vnode_master).
+
+
+
+% %% @doc Pings a random vnode to make sure communication is functional
+% ping()->
+%   ping(os:timestamp()).
 
 ping(Key) ->
   sync_command(Key, ping).

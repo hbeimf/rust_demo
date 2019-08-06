ea714d87ca109ef383175c3b102be6cad5c350aa
mm <mm@mail.com>
2019-08-06 16:11:43 +0800

    pu

--- leveldb_demo/apps/leveldbc/src/demo.erl
+++ leveldb_demo/apps/leveldbc/src/demo.erl
@@ -4,5 +4,22 @@
 -include("log.hrl").
 
 test() ->
+	Ref = open(),
+	Key = <<"test_key">>,
+	eleveldb:put(Ref, Key, <<"test_val">>, []),
+	R = eleveldb:get(Ref, Key, []),
+	?LOG({r, R}),
+	eleveldb:close(Ref).
+	
+open() ->
+	% {"../riak_learn_data/cluster_meta/trees",
+             Options = [{create_if_missing,true},
+              {is_internal_db,true},
+              {use_bloomfilter,true},
+              {write_buffer_size,7913116}],
+ 	DataDir = "./data",
 
-	ok.
+	{ok, Ref} = eleveldb:open(DataDir, Options),
+	Ref.
+
+

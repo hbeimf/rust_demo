2e53210e5b56b18c13d41dd79ed3f53b6a31614b
mm <mm@mail.com>
2019-08-09 14:15:55 +0800

    pu

--- mysqlc_demo/apps/mysqlc/src/demo.erl
+++ mysqlc_demo/apps/mysqlc/src/demo.erl
@@ -1,19 +1,36 @@
 % demo.erl
 -module(demo).
+
+-include_lib("glib/include/log.hrl").
 -compile(export_all).
 
 
 test() -> 
 
+	select1(),
+	select2(),
+	select3(),
 
 	ok.
 
 
 
 
+select1() -> 
+	Sql = "show tables",
+	R = mysqlc_comm:select(1, Sql),
+	?LOG(R),
+	ok.
 
+select2() -> 
+	Sql = "show tables",
+	R = mysqlc_comm:select(2, Sql),
+	?LOG(R),
+	ok.
 
-
-
-
+select3() -> 
+	Sql = "show tables",
+	R = mysqlc_comm:select(3, Sql),
+	?LOG(R),
+	ok.
 

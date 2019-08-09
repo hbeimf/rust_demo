8fbcff9878aaf2e09559b9d10e3654cd97b11582
mm <mm@mail.com>
2019-08-09 16:07:29 +0800

    pu

---
+++ php_demo/apps/glib/test/glib_SUITE.erl
@@ -0,0 +1,32 @@
+
+-module(glib_SUITE).
+-compile(export_all).
+
+
+% -include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+all() ->
+	[test1].
+
+test1(_) ->
+	io:format("test... ~n"),
+	ok.
+
+
+
+
+% test_fail(_) ->
+% 	% io:format("hello test~n"),
+% 	Reply = demo:hello(),
+% 	% io:format("reply: ~p~n", [Reply]),
+% 	?assert(ok == Reply),
+% 	% io:format("test... ~n"),
+% 	false.
+
+% test3(_) ->
+% 	io:format("test... ~n"),
+% 	ok.
+
+% eunit(_) ->
+% 	ok = eunit:test({application, erl_test}).
---
+++ php_demo/apps/php/src/demo.erl
@@ -0,0 +1,10 @@
+% demo.erl
+-module(demo).
+-compile(export_all).
+
+main_test() ->
+    % ?assertEqual(0, ephp:main(["test/code/test_empty.php"])).
+    Root = glib:root_dir(),
+    PhpFile = lists:concat([Root, "php_code/test.php"]),
+    ephp:main([PhpFile]),
+    ok.

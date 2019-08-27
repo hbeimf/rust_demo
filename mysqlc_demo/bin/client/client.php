f329b79cd0ae3acaceb0d6ad99a50e5d5af68dc0
mm <mm@mail.com>
2019-08-27 16:01:40 +0800

    pu

---
+++ mysqlc_demo/bin/client/client.php
@@ -0,0 +1,48 @@
+<?php
+include 'vendor/autoload.php';
+
+use Thrift\Protocol\TBinaryProtocol;
+use Thrift\Transport\TBufferedTransport;
+use Thrift\Transport\TSocket;
+
+require_once dirname(__FILE__) . '/msg/MsgService.php';
+require_once dirname(__FILE__) . '/msg/Types.php';
+
+class ThriftClient {
+	function Select($pool_id, $sql) {
+		$msg = new \msg\SelectReq(['pool_id' => $pool_id, 'sql' => $sql]);
+		return $this->client->Select($msg);
+	}
+
+	function QuerySql($pool_id, $sql) {
+		$msg = new \msg\QueryReq(['pool_id' => $pool_id, 'sql' => $sql]);
+		return $this->client->QuerySql($msg);
+	}
+
+	function __construct() {
+		$socket = new TSocket($this->_host, $this->_port);
+		$this->transport = new TBufferedTransport($socket, 1024, 1024);
+		$protocol = new TBinaryProtocol($this->transport);
+		$this->client = new \msg\MsgServiceClient($protocol);
+		$this->transport->open();
+	}
+	function __destruct() {
+		$this->transport->close();
+	}
+	public $transport = null;
+	public $client = null;
+	private $_host = "localhost";
+	private $_port = 9090;
+}
+
+$client = new ThriftClient();
+$pool_id = 1;
+$sql = "INSERT INTO `test` (`tx`) VALUES ('2')";
+$r = $client->QuerySql($pool_id, $sql);
+print_r($r);
+
+$select = "select * from test limit 3";
+$r = $client->Select($pool_id, $select);
+print_r($r);
+
+?>
---
+++ mysqlc_demo/bin/client/msg.thrift
@@ -0,0 +1,44 @@
+namespace * msg
+
+struct Message {
+  1:  i64 id,
+  2:  string text
+}
+
+// query start ==============================
+struct QueryReq {
+  1:  i64 pool_id,
+  2:  string sql
+}
+
+struct QueryReply {
+  1:  i64 code,
+  2:  string msg
+  3:  string result
+}
+
+// query end ================================
+
+
+// select  start ================================
+struct SelectReq {
+  1:  i64 pool_id, // 连接编号
+  2:  string sql
+}
+
+// select 响应
+struct SelectReply {
+  1:  i64 code,  // 返回码， 1：成功， 其它失败
+  2:  string msg,  // 返回描述
+  3:  string result,  // 查询结果， json
+}
+// select end =================================
+
+service MsgService {
+  // select
+  SelectReply Select(1: SelectReq q)
+
+ // api
+  QueryReply QuerySql(1: QueryReq q)
+
+}

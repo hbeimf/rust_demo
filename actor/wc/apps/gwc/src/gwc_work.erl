929ca6ce080bc0d170e11e7e2775159f82fef2f7
mm <mm@mail.com>
2019-10-12 12:17:59 +0800

    pu

---
+++ actor/wc/apps/gwc/src/gwc_work.erl
@@ -0,0 +1,92 @@
+%% gen_server代码模板
+
+-module(gwc_work).
+
+-behaviour(gen_server).
+% --------------------------------------------------------------------
+% Include files
+% --------------------------------------------------------------------
+
+% --------------------------------------------------------------------
+% External exports
+% --------------------------------------------------------------------
+-export([]).
+
+% gen_server callbacks
+-export([start_link/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+
+-define(LOG1(X), io:format("~n==========log1========{~p,~p}==============~n~p~n", [?MODULE,?LINE,X])).
+% -define(LOG1(X), true).
+
+% --------------------------------------------------------------------
+% External API
+% --------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+
+% --------------------------------------------------------------------
+% Function: init/1
+% Description: Initiates the server
+% Returns: {ok, State}          |
+%          {ok, State, Timeout} |
+%          ignore               |
+%          {stop, Reason}
+% --------------------------------------------------------------------
+init([]) ->
+	{ok, []}.
+
+% --------------------------------------------------------------------
+% Function: handle_call/3
+% Description: Handling call messages
+% Returns: {reply, Reply, State}          |
+%          {reply, Reply, State, Timeout} |
+%          {noreply, State}               |
+%          {noreply, State, Timeout}      |
+%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%          {stop, Reason, State}            (terminate/2 is called)
+% --------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+	Reply = ok,
+	{reply, Reply, State}.
+
+% --------------------------------------------------------------------
+% Function: handle_cast/2
+% Description: Handling cast messages
+% Returns: {noreply, State}          |
+%          {noreply, State, Timeout} |
+%          {stop, Reason, State}            (terminate/2 is called)
+% --------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+	{noreply, State}.
+
+% --------------------------------------------------------------------
+% Function: handle_info/2
+% Description: Handling all non call/cast messages
+% Returns: {noreply, State}           %          {noreply, State, Timeout} |
+%          {stop, Reason, State}            (terminate/2 is called)
+% --------------------------------------------------------------------
+handle_info(_Info, State) ->
+	{noreply, State}.
+
+
+% --------------------------------------------------------------------
+% Function: terminate/2
+% Description: Shutdown the server
+% Returns: any (ignored by gen_server)
+% --------------------------------------------------------------------
+terminate(_Reason, _State) ->
+	ok.
+
+% --------------------------------------------------------------------
+% Func: code_change/3
+% Purpose: Convert process state when code is changed
+% Returns: {ok, NewState}
+% --------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+	{ok, State}.
+
+
+% % private functions

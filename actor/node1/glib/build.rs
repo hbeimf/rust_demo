e7a5830783b7ffbbd86ae6b4cd94f642449302f2
mm <mm@mail.com>
2019-10-15 14:37:53 +0800

    push

--- actor/node1/glib/build.rs
+++ actor/node1/glib/build.rs
@@ -1,14 +1,14 @@
- extern crate protobuf_codegen_pure;
-
- fn generate_interop() {
-     protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
-         out_dir: "src/protos",
-         includes: &["protos"],
-         input: &["protos/msg.proto", "protos/gwc.proto"],
-         customize: Default::default(),
-     }).unwrap();
- }
+// extern crate protobuf_codegen_pure;
+//
+// fn generate_interop() {
+//     protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
+//         out_dir: "src/protos",
+//         includes: &["protos"],
+//         input: &["protos/msg.proto", "protos/gwc.proto"],
+//         customize: Default::default(),
+//     }).unwrap();
+// }
 
 fn main() {
-     generate_interop();
+//     generate_interop();
 }

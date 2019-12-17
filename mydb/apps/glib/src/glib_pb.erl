81e6036102dde3137d8e4e7bfaf0a13bfd8bcda0
mm <mm@mms-MacBook-Pro.local>
2019-12-17 21:12:27 +0400

    x

---
+++ mydb/apps/glib/src/glib_pb.erl
@@ -0,0 +1,22 @@
+% glib_pb.erl
+-module(glib_pb).
+-compile(export_all).
+
+
+
+
+-include_lib("glib/include/msg_proto.hrl").
+
+test() ->
+	Key = 123,
+	Str = <<"xx">>,
+	AesEncode = #'AesEncode'{
+                        key = Key,
+                        from = Str
+                    },
+	AesEncodeBin = msg_proto:encode_msg(AesEncode),
+	AesEncodeBin.
+
+
+
+

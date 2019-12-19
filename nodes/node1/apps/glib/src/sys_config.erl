f7b2e0c6a685ee3d7290836c1d04236b3ea888ce
mm <mm@mms-MacBook-Pro.local>
2019-12-19 15:32:18 +0400

    pu

--- nodes/node1/apps/glib/src/sys_config.erl
+++ nodes/node1/apps/glib/src/sys_config.erl
@@ -10,7 +10,7 @@
 % --------------------------------------------------------------------
 % External exports
 % --------------------------------------------------------------------
--export([]).
+-export([stop/0]).
 
 % gen_server callbacks
 -export([start_link/0]).
@@ -117,6 +117,10 @@ get_config(Sec, Key) ->
 			false
 	end. 	
 
+
+stop() -> 
+		gen_server:call(?MODULE, stop).
+
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
@@ -130,6 +134,8 @@ start_link() ->
 %          {stop, Reason}
 % --------------------------------------------------------------------
 init([]) ->
+	glib:write_req(start_sys_config, "debug", true),
+
 	?MODULE = ets:new(?SYS_CONFIG, ?ETS_OPTS),
 
  %    case read_config_file() of
@@ -161,6 +167,11 @@ init([]) ->
 %          {stop, Reason, Reply, State}   | (terminate/2 is called)
 %          {stop, Reason, State}            (terminate/2 is called)
 % --------------------------------------------------------------------
+
+handle_call(stop, _From, State) ->
+    % Reply = ok,
+    {stop, normal, State};
+
 handle_call(_Request, _From, State) ->
     Reply = ok,
     {reply, Reply, State}.
@@ -198,6 +209,7 @@ handle_info(_Info, State) ->
 % Returns: any (ignored by gen_server)
 % --------------------------------------------------------------------
 terminate(_Reason, _State) ->
+	glib:write_req(stop_sys_config, "debug", true),
     ok.
 
 % --------------------------------------------------------------------

30c0c34cd7ee939ab619708943bb437de1bfecbe
mm <mm@mms-MacBook-Pro.local>
2019-12-25 19:18:32 +0400

    pu

--- erlango/apps/glib/src/glib_pb.erl
+++ erlango/apps/glib/src/glib_pb.erl
@@ -26,3 +26,93 @@ decode_TestMsg(DataBin) ->
             , nick_name = NickName
             , phone = Phone} = msg_proto:decode_msg(DataBin,'TestMsg'),
 	{Name, NickName, Phone}.
+
+% // aes 加密
+% message AesEncode{   
+%     string  key = 1;
+%     string  from = 2;
+% }
+encode_AesEncode(Key, From)->
+	AesEncode = #'AesEncode'{
+		key = Key
+        , from = From
+	},
+	Pb = msg_proto:encode_msg(AesEncode),
+	Pb.
+decode_AesEncode(DataBin) -> 
+	#'AesEncode'{key = Key
+            , from = From
+		} = msg_proto:decode_msg(DataBin,'AesEncode'),
+	{Key, From}.
+
+% // aes 解密
+% message AesDecode{   
+%     string  key = 1;
+%     string  from = 2;
+% }
+encode_AesDecode(Key, From)->
+	AesDecode = #'AesDecode'{
+		key = Key
+        , from = From
+	},
+	Pb = msg_proto:encode_msg(AesDecode),
+	Pb.
+decode_AesDecode(DataBin) -> 
+	#'AesDecode'{key = Key
+            , from = From
+		} = msg_proto:decode_msg(DataBin,'AesDecode'),
+	{Key, From}.
+
+% // aes 解密回复 
+% message AesDecodeReply{   
+%     int32  code = 1;   // 1:成功； 2:失败
+%     string  reply = 2;
+% }
+encode_AesDecodeReply(Code, Reply)->
+	AesDecodeReply = #'AesDecodeReply'{
+		code = Code
+        , reply = Reply
+	},
+	Pb = msg_proto:encode_msg(AesDecodeReply),
+	Pb.
+decode_AesDecodeReply(DataBin) -> 
+	#'AesDecodeReply'{code = Code
+            , reply = Reply
+		} = msg_proto:decode_msg(DataBin,'AesDecodeReply'),
+	{Code, Reply}.
+
+% message Payload{   
+%     string  key = 1;
+%     bytes  pack = 2;
+% }
+encode_Payload(Key, Pack)->
+	Payload = #'Payload'{
+		key = Key
+        , pack = Pack
+	},
+	Pb = msg_proto:encode_msg(Payload),
+	Pb.
+decode_Payload(DataBin) -> 
+	#'Payload'{key = Key
+            , pack = Pack
+		} = msg_proto:decode_msg(DataBin,'Payload'),
+	{Key, Pack}.
+
+% message RpcPackage{   
+%     string  key = 1;  
+%     int32 cmd = 2;
+%     bytes payload = 3;
+% }
+encode_RpcPackage(Key, Cmd, Payload)->
+	RpcPackage = #'RpcPackage'{
+		key = Key
+        , cmd = Cmd
+        , payload = Payload
+	},
+	Pb = msg_proto:encode_msg(RpcPackage),
+	Pb.
+decode_RpcPackage(DataBin) -> 
+	#'RpcPackage'{key = Key
+            , cmd = Cmd
+            , payload = Payload} = msg_proto:decode_msg(DataBin,'RpcPackage'),
+	{Key, Cmd, Payload}.

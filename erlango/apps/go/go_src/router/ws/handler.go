92395a9248cf4f02163acdc000639c5b1ee5c8d4
mm <mm@mms-MacBook-Pro.local>
2019-12-26 12:01:38 +0400

    pu

--- erlango/apps/go/go_src/router/ws/handler.go
+++ erlango/apps/go/go_src/router/ws/handler.go
@@ -36,8 +36,8 @@ var (
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+	ReadBufferSize:  102400,
+	WriteBufferSize: 102400,
 }
 
 // Client is a middleman between the websocket connection and the hub.

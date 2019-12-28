919b02e11fcccc7df68f78d867e51d587d73d86e
mm <mm@mms-MacBook-Pro.local>
2019-12-28 18:01:45 +0400

    pu

--- nodes/node2/apps/wss/src/wss_action.erl
+++ nodes/node2/apps/wss/src/wss_action.erl
@@ -5,14 +5,22 @@
 
 -include_lib("glib/include/log.hrl").
 -include_lib("sys_log/include/write_log.hrl").
+-include("rr.hrl").
+
 
 action(Package) -> 
-    {From, Cmd, ReqPackage} = binary_to_term(Package),
+    #request{from = From, req_cmd = Cmd, req_data = ReqPackage} = binary_to_term(Package),
     action(Cmd, ReqPackage, From),
     ok.
 
+
+% -record(reply, {
+% 	from, 
+%     reply_code,
+%     reply_data
+% }).
 action(1000, _, From) ->
-    Reply = term_to_binary({From, 1001, pong}),
+    Reply = term_to_binary(#reply{from = From, reply_code = 1001, reply_data = pong}),
     self() ! {send, Reply},
     ok;
 action(Cmd, ReqPackage, From) ->

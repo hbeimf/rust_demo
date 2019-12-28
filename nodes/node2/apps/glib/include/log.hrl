cd7b91f95d73f60991809657b9f829f2c462c650
mm <mm@mms-MacBook-Pro.local>
2019-12-28 13:53:46 +0400

    x

---
+++ nodes/node2/apps/glib/include/log.hrl
@@ -0,0 +1,3 @@
+-define(LOG(X), io:format("~n==========log begin========{~p,~p}==============~n~p~n~n", [?MODULE,?LINE,X])).
+% -define(LOG(X), true).
+
---
+++ nodes/node2/apps/glib/src/glib_sup.erl
@@ -0,0 +1,52 @@
+%%%-------------------------------------------------------------------
+%% @doc glib top level supervisor.
+%% @end
+%%%-------------------------------------------------------------------
+
+-module(glib_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+
+%%====================================================================
+%% Supervisor callbacks
+%%====================================================================
+
+%% Child :: {Id,StartFunc,Restart,Shutdown,Type,Modules}
+init([]) ->
+    Children = children(),
+
+    {ok, { {one_for_one, 10, 10}, Children} }.
+%%====================================================================
+%% Internal functions
+%%====================================================================
+children() -> 
+  [
+      child(sys_config)
+      , child(glib_cluster_actor)
+  ].
+
+
+child(Mod) ->
+	Child = {Mod, {Mod, start_link, []},
+               permanent, 5000, worker, [Mod]},
+               Child.
+
+child_sup(Mod) ->
+              Child = {Mod, {Mod, start_link, []},
+               permanent, 5000, supervisor, [Mod]},
+               Child. 

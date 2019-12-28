4c05975232a8e06cc6bafacb54604aba7f042599
mm <mm@mms-MacBook-Pro.local>
2019-12-28 12:07:09 +0400

    d

--- nodes/node1/apps/esnowflake/include/esnowflake.hrl
+++
@@ -1,5 +0,0 @@
--define(TWEPOCH, 1508980320000). %% 2017-10-26 01:12:00 (UTC)
--define(DEFAULT_WORKER_MIN_MAX, [0, 9]).
-
--define(LOG(X), io:format("~n==========log begin========{~p,~p}==============~n~p~n~n", [?MODULE,?LINE,X])).
-% -define(LOG(X), true).

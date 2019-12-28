6cfb0abd522f3e4989535476bbe55ea863d05453
mm <mm@mms-MacBook-Pro.local>
2019-12-28 14:05:02 +0400

    pu

--- nodes/node2/apps/wss/src/wss_app.erl
+++ nodes/node2/apps/wss/src/wss_app.erl
@@ -15,6 +15,20 @@
 %%====================================================================
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+		{'_', [
+			{"/ws", ws_handler_game_server, []},
+			{"/websocket", ws_handler_gateway, []}
+		]}
+	]),
+
+	{ok, ConfigList} = sys_config:get_config(http),
+	% {_, {host, Host}, _} = lists:keytake(host, 1, ConfigList),
+	{_, {port, Port}, _} = lists:keytake(port, 1, ConfigList),
+
+
+	{ok, _} = cowboy:start_http(http, 100, [{port, Port}],
+		[{env, [{dispatch, Dispatch}]}]),
     wss_sup:start_link().
 
 %%--------------------------------------------------------------------

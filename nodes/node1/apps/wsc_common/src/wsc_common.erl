3d61d00d341521f3f463adb9bf95fc1ebe729a1d
mm <mm@mms-MacBook-Pro.local>
2020-01-02 11:53:50 +0400

    pu

--- nodes/node1/apps/wsc_common/src/wsc_common.erl
+++ nodes/node1/apps/wsc_common/src/wsc_common.erl
@@ -14,13 +14,36 @@
 -include_lib("sys_log/include/write_log.hrl").
 
 start_pool() ->
-  start_pool(1).
+  lists:foreach(fun(Id)->
+    start_pool(Id)
+                end, lists:seq(1, 10)).
 
 start_pool(PoolId) ->
   wsc_common_pool_sup:start_wsc_pool(PoolId).
 
+
+pool_name(1)->
+  pool_1;
+pool_name(2)->
+  pool_2;
+pool_name(3)->
+  pool_3;
+pool_name(4)->
+  pool_4;
+pool_name(5)->
+  pool_5;
+pool_name(6)->
+  pool_6;
+pool_name(7)->
+  pool_7;
+pool_name(8)->
+  pool_8;
+pool_name(9)->
+  pool_9;
+pool_name(10)->
+  pool_10;
 pool_name(_PoolId)->
-  pool_1.
+  pool_100.
 
 pool_addr(_PoolId) ->
   "ws://localhost:5678/ws".

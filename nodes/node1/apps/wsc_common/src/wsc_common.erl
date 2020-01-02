2d3a5ef895bb00270752265a2273aee9057c986d
mm <mm@mms-MacBook-Pro.local>
2020-01-02 10:04:42 +0400

    pu

--- nodes/node1/apps/wsc_common/src/wsc_common.erl
+++ nodes/node1/apps/wsc_common/src/wsc_common.erl
@@ -13,6 +13,12 @@
 -include_lib("glib/include/log.hrl").
 -include_lib("sys_log/include/write_log.hrl").
 
+start_pool() ->
+  start_pool(1).
+
+start_pool(PoolId) ->
+  wsc_common_sup:start_wsc_pool(PoolId).
+
 pool_name(_)->
   pool_1.
 

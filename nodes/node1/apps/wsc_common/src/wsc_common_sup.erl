dcaad294464895c3229b1fe5d6f619ffc05de54b
mm <mm@mms-MacBook-Pro.local>
2020-01-04 12:21:20 +0400

    pu

--- nodes/node1/apps/wsc_common/src/wsc_common_sup.erl
+++ nodes/node1/apps/wsc_common/src/wsc_common_sup.erl
@@ -53,3 +53,15 @@ child_sup(Mod) ->
     Child = {Mod, {Mod, start_link, []},
         permanent, 5000, supervisor, [Mod]},
     Child.
+
+
+%%Restart = permanent | transient | temporary
+
+%%Restart 这个参数用来告诉supervisor，当该child process挂掉时，是否能够重启它，
+%%permanent表示永远可以（不管child process是以何种原因挂掉），
+%%temporary表示永远不可以（即挂掉了将不再重启），
+%%transient 有点特殊，它表示child process若是因为normal或者shutdown原因结束，
+%%则不再重启，否则可以restart（ps:Restart参数设置会覆盖Restart Strategy，
+%%譬如一个child process的Restart设置为temporary，supervisor的Restart Strategy
+%%是one_for_all，那么当其他某个child process挂掉后，将会导致该child process(temporay)
+%%被terminate并且不再被重启）

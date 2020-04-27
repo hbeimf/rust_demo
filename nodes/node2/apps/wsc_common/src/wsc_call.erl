dd8afe4e0ddfe06079e5d3604598a3881704bc54
maomao <you@example.com>
2020-04-27 16:12:00 +0800

    pu

---
+++ nodes/node2/apps/wsc_common/src/wsc_call.erl
@@ -0,0 +1,22 @@
+% wsc_common_call.erl
+-module(wsc_call).
+
+-compile(export_all).
+
+-include_lib("glib/include/log.hrl").
+-include_lib("sys_log/include/write_log.hrl").
+-include_lib("glib/include/rr.hrl").
+
+-define(TIMEOUT, 5000).
+
+test_call() -> 
+	ReqPackage = {glib, replace, ["helloworld", "world", " you"]},
+  	% R = pools:call(PoolId, call_fun, ReqPackage),
+	call({1, call_fun, ReqPackage}).
+
+call({PoolId, Cmd, ReqPackage}) -> 
+	{ok, Pid} = pools_call_sup:start_actor(),
+	R = gen_server:call(Pid, {call, PoolId, Cmd, ReqPackage}, ?TIMEOUT),
+	% gen_server:call(Worker, {call, Cmd, ReqPackage}, ?TIMEOUT)
+	% ?LOG(R),
+	R.

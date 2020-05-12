0fef98fcf1d5e16c4ef43ad20a41ec067b493354
maomao <you@example.com>
2020-05-12 10:11:53 +0800

    pu

--- nodes/node4/apps/game_server/lib/game_server/application.ex
+++ nodes/node4/apps/game_server/lib/game_server/application.ex
@@ -6,6 +6,7 @@ defmodule GameServer.Application do
   use Application
 
   def start(_type, _args) do
+    start_game_server()
     # List all child processes to be supervised
     children = [
       # Starts a worker by calling: GameServer.Worker.start_link(arg)
@@ -16,5 +17,21 @@ defmodule GameServer.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: GameServer.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  def start_game_server() do 
+    dispatch = :cowboy_router.compile([
+      {:_, [
+        {'/ws', :ws_handler, []},	
+        {'/api/gm/statue', :handler_statue, []}	
+      ]}
+    ])
+  
+    {:ok, config_list} = :sys_config.get_config(:http)
+    {_, {:port, port}, _} = :lists.keytake(:port, 1, config_list)
+  
+  
+    {:ok, _} = :cowboy.start_http(:http, 100, [{:port, port}, {:max_connections, 1000000}], [{:env, [{:dispatch, dispatch}]}])
+    :ok
   end
 end

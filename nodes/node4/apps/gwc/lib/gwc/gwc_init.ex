7706a04df2ff955a0c523e01b5aaa4ed3ddc03e9
maomao <you@example.com>
2020-05-12 14:29:09 +0800

    pu

--- nodes/node4/apps/gwc/lib/gwc/gwc_init.ex
+++ nodes/node4/apps/gwc/lib/gwc/gwc_init.ex
@@ -39,6 +39,21 @@ defmodule Gwc.GwcInit do
     #     Msg = glib_pb:encode_Msg(?CMD_REGISTER, Register),
     #     Pid ! {init_send, Msg},
     #     register_gw_2_gwc(OtherWork, Size, WorkId+1).
+    def register_gw_2_gwc([], _, _) do 
+        :ok
+    end
+    def register_gw_2_gwc([{_, pid, _, _}|other_work], size, work_id) do 
+        #     RegisterConfig = register_config(Size, WorkId),
+        register_config = register_config(size, work_id)
+        #     Register = wsc_common:req(register_gw, RegisterConfig),
+        register = :wsc_common.req(:register_gw, register_config)
+        #     Msg = glib_pb:encode_Msg(?CMD_REGISTER, Register),
+        msg = :glib_pb.encode_Msg(10000, register)
+        #     Pid ! {init_send, Msg},
+        send pid, {:init_send, msg}
+        #     register_gw_2_gwc(OtherWork, Size, WorkId+1).
+        register_gw_2_gwc(other_work, size, work_id + 1) 
+    end
       
     #   regiter_gw_2_gwc() ->
     #     ConfigList = glib_config:hubs(),
@@ -47,9 +62,14 @@ defmodule Gwc.GwcInit do
     #       register_gw_2_gwc(Works, erlang:length(Works), 1)
     #     end ,ConfigList),
     #     ok.
-    def regiter_gw_2_gwc() do 
-
-    end  
+    def regiter_gw_2_gwc([], _, _) do 
+        config_list = :glib_config.hubs()
+        :lists.foreach(fn(%{pool_id: v_pool_id, addr: v_addr}) -> 
+            works = :wdc_common.works(v_pool_id)
+            register_gw_2_gwc(works, :erlang.length(works), 1)
+        end, config_list)
+    end
+    
       
 
     #   start_pool() -> 
@@ -80,8 +100,14 @@ defmodule Gwc.GwcInit do
     #       , size => Size
     #       , work_id => WorkId
     #     }.
-    def register_config() do 
-
+    def register_config(size, work_id) do 
+        cluster_id = cluster(:sys_config.get_config(:node, :cluster_id))
+        %{
+            :cluster_id => cluster_id,
+            :node_id => :sys_config.get_config(:node, :node_id),
+            :size => size,
+            :work_id => work_id
+        }
     end
 
       
@@ -107,9 +133,21 @@ defmodule Gwc.GwcInit do
     #     pool_gw_10;
     #   cluster(_) ->
     #     pool_gw_100.
-    def cluster() do 
 
-    end
+    # Gwc.GwcInit.cluster(2)
+    def cluster(1), do: :pool_gw_1 
+    def cluster(2), do: :pool_gw_2
+    def cluster(3), do: :pool_gw_3 
+    def cluster(4), do: :pool_gw_4 
+    def cluster(5), do: :pool_gw_5 
+    def cluster(6), do: :pool_gw_6 
+    def cluster(7), do: :pool_gw_7 
+    def cluster(8), do: :pool_gw_8 
+    def cluster(9), do: :pool_gw_9 
+    def cluster(10), do: :pool_gw_10 
+    def cluster(_), do: :pool_gw_100 
+        
+
         
 
   end

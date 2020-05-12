7a4b5d8001805033726af870eef7a627a963fb94
maomao <you@example.com>
2020-05-12 17:58:58 +0800

    pu

---
+++ nodes/node5/apps/app_ex/elixir_libs/elixir_app_test/lib/elixir_app_test.ex
@@ -0,0 +1,18 @@
+defmodule ElixirAppTest do
+  @moduledoc """
+  Documentation for ElixirAppTest.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirAppTest.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ nodes/node5/apps/app_ex/elixir_libs/elixir_app_test/lib/elixir_app_test/application.ex
@@ -0,0 +1,20 @@
+defmodule ElixirAppTest.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: ElixirAppTest.Worker.start_link(arg)
+      # {ElixirAppTest.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: ElixirAppTest.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end

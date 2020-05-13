83624111db01404a03a518eed0caa480e35e6893
maomao <you@example.com>
2020-05-13 21:44:33 +0800

    x

--- nif/nif_fun.c
+++ nif/nif_fun.c
@@ -10,15 +10,15 @@ static ERL_NIF_TERM hello(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 // test_so_func
 static ERL_NIF_TERM add(ErlNifEnv *env, int argc, ERL_NIF_TERM argv[])
 {
-        int a;
-        int b;
-        if(!enif_get_int(env, argv[0], &a))
-                return enif_make_badarg(env);
+	int a;
+	int b;
+	if(!enif_get_int(env, argv[0], &a))
+		return enif_make_badarg(env);
 
-        if(!enif_get_int(env, argv[1], &b))
-                return enif_make_badarg(env);
+	if(!enif_get_int(env, argv[1], &b))
+		return enif_make_badarg(env);
 
-        ERL_NIF_TERM res = enif_make_int(env, 0);
+	ERL_NIF_TERM res = enif_make_int(env, 0);
 
 	void* handle;
 	typedef int (*FPTR)(int,int);
@@ -28,25 +28,9 @@ static ERL_NIF_TERM add(ErlNifEnv *env, int argc, ERL_NIF_TERM argv[])
 
 	int result = (*fptr)(a,b);
 
-        // int r;
-        // r = a + b;
-        // res = enif_make_list_cell(env, enif_make_int(env, result), res);
 	res = enif_make_int(env, result);
-        return res;
+	return res;
 
-
-
-        // else
-        // {
-        //         int i;
-        //         ERL_NIF_TERM res = enif_make_list(env, 0);
-        //         for(i = 2; i < n; ++i)
-        //         {
-        //                 if(isPrime(i))
-        //                         res = enif_make_list_cell(env, enif_make_int(env, i), res);
-        //         }
-        //         return res;
-        // }
 }
 
 static ErlNifFunc nif_funcs[] =
@@ -56,5 +40,3 @@ static ErlNifFunc nif_funcs[] =
 };
 
 ERL_NIF_INIT(nif_fun, nif_funcs,NULL,NULL,NULL,NULL)
-
-// gcc -fPIC -shared -o nif_fun.so nif_fun.c -I /usr/local/erlang_18.3/lib/erlang/usr/include

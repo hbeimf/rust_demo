88b638d558335376b81469b71bf01766e1947f4a
maomao <you@example.com>
2020-05-14 19:18:53 +0800

    pu

--- nif/fc.c
+++ nif/fc.c
@@ -187,12 +187,66 @@ static ERL_NIF_TERM set_difficulty(ErlNifEnv *env, int argc, ERL_NIF_TERM argv[]
 // % bool
 // set_place_type(_TableId_Int, _Pt_Int) -> 
 // 	"NIF library not loaded".
+// //设置场地类型(起伏)0：小起伏，1：中等起伏，2：大起伏
+// extern "C" bool SetPlaceType(int tableid, int pt);
+static ERL_NIF_TERM set_place_type(ErlNifEnv *env, int argc, ERL_NIF_TERM argv[])
+{
+	int tableId;
+	int pt;
+	
+	if(!enif_get_int(env, argv[0], &tableId))
+		return enif_make_badarg(env);
 
+	if(!enif_get_int(env, argv[1], &pt))
+		return enif_make_badarg(env);
+
+	ERL_NIF_TERM res = enif_make_int(env, 0);
+
+	void* handle;
+	typedef int (*FPTR)(int, int);
+
+	handle = dlopen("./fishcontrol.so", 1);
+	FPTR fptr = (FPTR)dlsym(handle, "SetPlaceType");
+
+	// int result = (*fptr)(filePath, tableId);
+	int result = (*fptr)(tableId, pt);
+
+	// res = enif_make_int(env, result);
+	return res;
+}
 
 // % bool
 // chou_fang(_TableId_Int, _CoinNum_Int) -> 
 // 	"NIF library not loaded".
+// //抽放接口，coinNum为人民币，大于0表示放水coinNum元，小于0表示抽水
+// extern "C" bool ChouFang(int tableid, int coinNum);
+static ERL_NIF_TERM chou_fang(ErlNifEnv *env, int argc, ERL_NIF_TERM argv[])
+{
+	int tableId;
+	int coinNum;
+	
+	if(!enif_get_int(env, argv[0], &tableId))
+		return enif_make_badarg(env);
 
+	if(!enif_get_int(env, argv[1], &coinNum))
+		return enif_make_badarg(env);
+
+	ERL_NIF_TERM res = enif_make_int(env, 0);
+
+	void* handle;
+	typedef int (*FPTR)(int, int);
+
+	handle = dlopen("./fishcontrol.so", 1);
+	FPTR fptr = (FPTR)dlsym(handle, "ChouFang");
+
+	// int result = (*fptr)(filePath, tableId);
+	int result = (*fptr)(tableId, coinNum);
+
+	// res = enif_make_int(env, result);
+	return res;
+}
+// //测试用，获取内部数据，用于显示
+// extern "C" const char* GetInnerData(int tableid);
 // % char
 // get_inner_data(_TableId_Int) -> 
 // 	"NIF library not loaded".
@@ -205,7 +259,9 @@ static ErlNifFunc nif_funcs[] =
 	{"save_to_file", 1, save_to_file},
 	{"save_and_release", 1, save_and_release},
 	{"catch_fish", 4, catch_fish},
-	{"set_difficulty", 3, set_difficulty}
+	{"set_difficulty", 3, set_difficulty},
+	{"set_place_type", 2, set_place_type},
+	{"chou_fang", 2, chou_fang}
 };
 
 ERL_NIF_INIT(fc, nif_funcs,NULL,NULL,NULL,NULL)

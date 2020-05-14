9255f33c15f237eb2e4068a9a34c179b3f0e9c32
maomao <you@example.com>
2020-05-14 20:12:55 +0800

    pu

--- nodes/node1/apps/fish_control/priv/fc.c
+++ nodes/node1/apps/fish_control/priv/fc.c
@@ -7,52 +7,15 @@ static ERL_NIF_TERM hello(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 	return enif_make_string(env, "Hello world!", ERL_NIF_LATIN1);
 }
 
-// // test_so_func
-// static ERL_NIF_TERM add(ErlNifEnv *env, int argc, ERL_NIF_TERM argv[])
-// {
-// 	int a;
-// 	int b;
-// 	if(!enif_get_int(env, argv[0], &a))
-// 		return enif_make_badarg(env);
-
-// 	if(!enif_get_int(env, argv[1], &b))
-// 		return enif_make_badarg(env);
-
-// 	ERL_NIF_TERM res = enif_make_int(env, 0);
-
-// 	void* handle;
-// 	typedef int (*FPTR)(int,int);
-
-// 	handle = dlopen("./test_so.so", 1);
-// 	FPTR fptr = (FPTR)dlsym(handle, "add");
-
-// 	int result = (*fptr)(a,b);
-
-// 	res = enif_make_int(env, result);
-// 	return res;
-
-// }
-
-
 // // % =====================================
 // // % bool
 // // create_fish_control(_FilePath_Char, _TableId_Int) ->
 // // 	"NIF library not loaded".
 static ERL_NIF_TERM create_fish_control(ErlNifEnv *env, int argc, ERL_NIF_TERM argv[])
 {
-	// char db_name[4096];
-
-	//     if(!enif_get_string(env, argv[1], db_name, sizeof(db_name), ERL_NIF_LATIN1) ||
-	//        !enif_is_list(env, argv[2]))
-	//     {
-	//         return enif_make_badarg(env);
-	//     }   // if
-
 
 	char filePath[4096];
 	int tableId;
-	// if(!enif_get_string(env, argv[0], &filePath))
-	// 	return enif_make_badarg(env);
 
 	if(!enif_get_string(env, argv[0], filePath, sizeof(filePath), ERL_NIF_LATIN1))
 	{
